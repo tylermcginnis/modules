923f9a729382c1d4efed0dfb40397b68e857be2a
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-08-23 21:41:25 -0700

    wrappers

--- dom.js
+++ dom.js
@@ -1,21 +1,25 @@
-function addUserToDOM(name) {
-  const node = document.createElement("li")
-  const text = document.createTextNode(name)
-  node.appendChild(text)
+function domWrapper() {
+  function addUserToDOM(name) {
+    const node = document.createElement("li")
+    const text = document.createTextNode(name)
+    node.appendChild(text)
 
-  document.getElementById("users")
-    .appendChild(node)
+    document.getElementById("users")
+      .appendChild(node)
+  }
+
+  document.getElementById("submit")
+    .addEventListener("click", function() {
+      var input = document.getElementById("input")
+      addUserToDOM(input.value)
+
+      input.value = ""
+  })
+
+  var users = APP.getUsers()
+  for (var i = 0; i < users.length; i++) {
+    addUserToDOM(users[i])
+  }
 }
 
-document.getElementById("submit")
-  .addEventListener("click", function() {
-    var input = document.getElementById("input")
-    addUserToDOM(input.value)
-
-    input.value = ""
-})
-
-var users = window.getUsers()
-for (var i = 0; i < users.length; i++) {
-  addUserToDOM(users[i])
-}
+domWrapper()
--- users.js
+++ users.js
@@ -2,4 +2,14 @@ var users = ["Tyler", "Sarah", "Dan"]
 
 function getUsers() {
   return users
+}function usersWrapper () {
+  var users = ["Tyler", "Sarah", "Dan"]
+
+  function getUsers() {
+    return users
+  }
+
+  APP.getUsers = getUsers
 }
+
+usersWrapper()
